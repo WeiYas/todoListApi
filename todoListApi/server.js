068abb7d8983d49f6c52affa4eb3f1db40981167
DESKTOP-AC1FI9D\yasmi <yasmine.weiwert@btsgp.lu>
2020-11-18 22:31:50 +0100

    mongoose installation again controllers added brackets to the if statement

--- todoListApi/server.js
+++ todoListApi/server.js
@@ -21,9 +21,8 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-console.log('todo list RESTful API server started on: ' + port);
-
 app.listen(port);
 
+console.log('todo list RESTful API server started on: ' + port);
 
 
